b2e74dd8dec83a7d88d5857c7573e5cebc174759
joowon <saven7788@gmail.com>
2017-04-10 17:58:27 +0900

    코드정리

--- routes/singer.js
+++ routes/singer.js
@@ -5,16 +5,6 @@ var fs = require('fs');
 var db_config = require('../config/db_config.json');
 var router = express.Router();
 
-
-var pool = mysql.createPool({
-  host : db_config.host,
-  port : db_config.port,
-  user : db_config.user,
-  database : db_config.database,
-  connectionLimit : db_config.connectionLimit
-});
-
-
 router.get('/', function(req, res, next) {
   res.render('singer', { title: 'singer' });
 });
@@ -151,7 +141,7 @@ router.post('/singerAdd', function(req, res, next){
     		}
             res.status(200).send({result : "success"});
 
-        // });
+         });
     });// pool
 });//post
 
