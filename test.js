03d960af1264433ee746e32d73e71c00de876c36
joowon <saven7788@gmail.com>
2017-05-10 01:46:06 +0900

    test

--- test.js
+++ test.js
@@ -1,20 +1,53 @@
+//
+//
+// const EventEmitter = require('events');
+//
+//
+//
+// const myEmitter = new EventEmitter();
+// // Only do this once so we don't loop forever
+// myEmitter.once('newListener', (event, listener) => {
+//   if (event === 'event') {
+//     // Insert a new listener in front
+//     myEmitter.on('event', () => {
+//       console.log('B');
+//     });
+//   }
+// });
+// myEmitter.on('event', () => {
+//   console.log('A');
+// });
+// myEmitter.emit('event');
+//
 
 
-const EventEmitter = require('events');
+var express = require('express');
+var storage = require('@google-cloud/storage');
+var fs = require('fs');
 
+var router = express.Router();
+var gcs = storage({
+	projectId: 'favorable-mark-151209',
+	keyFilename : '../config/keyfile.json'
 
-
-const myEmitter = new EventEmitter();
-// Only do this once so we don't loop forever
-myEmitter.once('newListener', (event, listener) => {
-  if (event === 'event') {
-    // Insert a new listener in front
-    myEmitter.on('event', () => {
-      console.log('B');
-    });
-  }
 });
-myEmitter.on('event', () => {
-  console.log('A');
-});
-myEmitter.emit('event');
+
+console.log("1");
+var bucket = gcs.bucket('duckmate_1');
+
+// download a file from your bucket.
+
+bucket.file('duckmate_1/new/why.jpeg').download({
+	destination: '../photos/why.jpeg'},function(err) {console.log("1",err)});
+
+
+// bucket.upload('../photos/logo.png',function(err,file){
+//
+// 	console.log(err);
+// 	if(!err){
+// 	 	console.log("success");
+// 	}
+// });
+
+
+module.exports = router;
