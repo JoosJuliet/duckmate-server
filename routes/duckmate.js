8499d2499250b8e20a3d66e35aa29e4d48538efe
joowon <saven7788@gmail.com>
2017-05-10 12:42:25 +0900

    test

--- routes/duckmate.js
+++ routes/duckmate.js
@@ -6,9 +6,40 @@ var router = express.Router();
 
 var app = express();
 
-router.post('/profile', upload.single('test.png'), function (req, res, next) {
+router.post('/:profile', upload.single('test.png'), function (req, res, next) {
 
-    console.log(req.file);
+    var upload = function (req, res) {
+  var deferred = Q.defer();
+  var storage = multer.diskStorage({
+    // 서버에 저장할 폴더
+    destination: function (req, file, cb) {
+      cb(null, imagePath);
+    },
+
+    // 서버에 저장할 파일 명
+    filename: function (req, file, cb) {
+      file.uploadedFile = {
+        name: req.params.filename,
+        ext: file.mimetype.split('/')[1]
+      };
+      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
+    }
+  });
+
+  var upload = multer({ storage: storage }).single('file');
+  upload(req, res, function (err) {
+    if (err) deferred.reject();
+    else deferred.resolve(req.file.uploadedFile);
+  });
+  return deferred.promise;
+};
+
+upload(req, res).then(function (file) {
+    res.json(file);
+  }, function (err) {
+    res.send(500, err);
+  });
+
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
 })
