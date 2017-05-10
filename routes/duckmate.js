6f7d091b2d2bcbf82d3e8f74888652bd7363e414
joowon <saven7788@gmail.com>
2017-05-10 16:50:05 +0900

    refactoring filename

--- routes/duckmate.js
+++ routes/duckmate.js
@@ -10,36 +10,36 @@ var Q = require("q");
 router.post('/:filename',  function (req, res, next) {
 	console.log(req.file);
     var upload = function (req, res) {
-  var deferred = Q.defer();
-  var storage = multer.diskStorage({
-    // 서버에 저장할 폴더
-    destination: function (req, file, cb) {
-      cb(null, imagePath);
-    },
+        var deferred = Q.defer();
+        var storage = multer.diskStorage({
+        // 서버에 저장할 폴더
+            destination: function (req, file, cb) {
+                cb(null, imagePath);
+            },
 
-    // 서버에 저장할 파일 명
-    filename: function (req, file, cb) {
-      file.uploadedFile = {
-        name: req.params.filename,
-        ext: file.mimetype.split('/')[1]
-      };
-      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
-    }
-  });
+        // 서버에 저장할 파일 명
+            filename: function (req, file, cb) {
+                file.uploadedFile = {
+                    name: req.params.filename,
+                    ext: file.mimetype.split('/')[1]
+                };
+                cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
+            }
+        });
 
-  var upload = multer({ storage: storage }).single('file');
-  upload(req, res, function (err) {
-    if (err) deferred.reject();
-    else deferred.resolve(req.file.uploadedFile);
-  });
-  return deferred.promise;
-};
+        var upload = multer({ storage: storage }).single('file');
+        upload(req, res, function (err) {
+            if (err) deferred.reject();
+            else deferred.resolve(req.file.uploadedFile);
+        });
+        return deferred.promise;
+    };
 
-upload(req, res).then(function (file) {
-    res.json(file);
-  }, function (err) {
-    res.send(500, err);
-  });
+    upload(req, res).then(function (file) {
+        res.json(file);
+    }, function (err) {
+        res.send(500, err);
+    });
 
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
