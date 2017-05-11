2ddc265ecd6155452b9b78f7590ce5abc1081b3e
joowon <saven7788@gmail.com>
2017-05-11 12:15:37 +0900

    test upload

--- test.js
+++ test.js
@@ -14,30 +14,27 @@ console.log("1");
 var bucket = gcs.bucket('duckmate_1');
 
 
-//douwnload
+	//douwnload
 bucket.file('why.png').download({
-  destination: './photos/why.png'
+	destination: './photos/why.png'
 }, function(err) {});
 
 
 bucket.upload('./public/images/profile.png', function(err, file) {
-  if (!err) {
-    // "zebra.jpg" is now in your bucket.
-  }
+	if (!err) {
+		console.log("success");
+	}
 });
 
+// Streams are also supported for reading and writing files.
+// var remoteReadStream = bucket.file('giraffe.jpg').createReadStream();
+// var localWriteStream = fs.createWriteStream('/photos/zoo/giraffe.jpg');
+// remoteReadStream.pipe(localWriteStream);
+//
+// var localReadStream = fs.createReadStream('/photos/zoo/zebra.jpg');
+// var remoteWriteStream = bucket.file('zebra.jpg').createWriteStream();
+// localReadStream.pipe(remoteWriteStream);
+//
+// module.exports = router;
 
 // automator 연동 api
-
-
-
-// Streams are also supported for reading and writing files.
-var remoteReadStream = bucket.file('giraffe.jpg').createReadStream();
-var localWriteStream = fs.createWriteStream('/photos/zoo/giraffe.jpg');
-remoteReadStream.pipe(localWriteStream);
-
-var localReadStream = fs.createReadStream('/photos/zoo/zebra.jpg');
-var remoteWriteStream = bucket.file('zebra.jpg').createWriteStream();
-localReadStream.pipe(remoteWriteStream);
-
-module.exports = router;
