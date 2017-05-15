cfe87854dcf24352863ce43fbf3f627ff067a107
joowon <saven7788@gmail.com>
2017-05-15 18:20:05 +0900

    merge conflict고치기

--- test.js
+++ test.js
@@ -28,13 +28,16 @@ var bucket = gcs.bucket('duckmate_1');
 // });
 
 /******************************************************/
-var remoteFile = bucket.file('test1.png');
-var localFilename = './photos/test1.png';
+
+
+/**************** Stream douwnload ver ******************/
+var remoteFile = bucket.file('test01.png');
+var localFilename = './photos/test01.png';
 
 remoteFile.createReadStream()
 	.on('error', function(err) {console.log("err",err);})
 	.on('response', function(response) {
-		console.log("response.status",response);
+		console.log("response.status",response.status);
 	// Server connected and responded with the specified status and headers.
 	})
 	.on('end', function() {
@@ -43,6 +46,34 @@ remoteFile.createReadStream()
 	})
 	.pipe(fs.createWriteStream(localFilename));
 
+/******************************************************/
+
+
+/**************** Stream upload ver ******************/
+
+	//pipe : 받아서 넘겨주는 것 pipe속에 있는 것 바꾸면된다.
+	// 리눅스에서 앞 결과 값 받아서 바로 전달 앞에값 리턴값을 메모리 그대로 뒤에 전해준다.
+	//createWriteStream을 햇을 때 그대로 보내준다
+	// 사용자에게 보내는 createStream해서 보내면 된다.
+
+
+//그냥 여기서 createStream부분으로 하기
+//default는 private이고 못바꾸기에 public으로 하기 -> 용도에 따라서
+// fs.createReadStream('/Users/stephen/Photos/birthday-at-the-zoo/panda.jpg')
+//   .pipe(file.createWriteStream({
+//     metadata: {
+//       contentType: 'image/jpeg',
+//       metadata: {
+//         custom: 'metadata'
+//       }
+//     }
+//   }))
+//   .on('error', function(err) {})
+//   .on('finish', function() {
+//     // The file upload is complete.
+//   });
+
+/**************************************************/
 
 
 
