9a816a00a704c9b27ad9c81ecba888d05e6fe79e
joowon <saven7788@gmail.com>
2017-05-19 19:36:32 +0900

    test

--- routes/duckmate.js
+++ routes/duckmate.js
@@ -7,49 +7,38 @@ var imagePath = "photos/images";
 var app = express();
 var Q = require("q");
 
-router.post('/:filename',  function (req, res, next) {
-
-        console.log("1");
-        console.log(req.params.filename);
-        console.log(req.file);
+router.post('/:filename', function(req, res, next) {
     var upload = function (req, res) {
-        var deferred = Q.defer();
-        var storage = multer.diskStorage({
-        // 서버에 저장할 폴더
-            destination: function (req, file, cb) {
-                cb(null, imagePath);
-            },
+  var deferred = Q.defer();
+  var storage = multer.diskStorage({
+    // 서버에 저장할 폴더
+    destination: function (req, file, cb) {
+      cb(null, imagePath);
+    },
 
-        // 서버에 저장할 파일 명
-            filename: function (req, file, cb) {
-                file.uploadedFile = {
-                    name: req.params.filename,
-                    ext: file.mimetype.split('/')[1]
-                };
-                cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
-            }
-        });
+    // 서버에 저장할 파일 명
+    filename: function (req, file, cb) {
+      file.uploadedFile = {
+        name: req.params.filename,
+        ext: file.mimetype.split('/')[1]
+      };
+      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
+    }
+  });
 
-        var upload = multer({ storage: storage }).single('file');
-        upload(req, res, function (err) {
-            if (err){
-                console.log("upload single file",err);
-                deferred.reject();
-            }else{ deferred.resolve(req.file.uploadedFile); }
-        });
-        return deferred.promise;
-    };
-
-    upload(req, res).then(function (file) {
-        res.json(file);
-    }, function (err) {
-		console.log("upload err",err);
-        res.send(500, err);
-    });
-
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-})
+  var upload = multer({ storage: storage }).single('file');
+  upload(req, res, function (err) {
+    if (err) deferred.reject();
+    else deferred.resolve(req.file.uploadedFile);
+  });
+  return deferred.promise;
+};
+  upload(req, res).then(function (file) {
+    res.json(file);
+  }, function (err) {
+    res.send(500, err);
+  });
+});
 
 //member_id로
 //UPDATE 테이블명 SET 바꿀것  WHERE 조건 Class=10
