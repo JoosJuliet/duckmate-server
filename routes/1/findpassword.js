8817011f7dc30bb910686747ec1abf732509fa11
joowon <saven7788@gmail.com>
2017-05-31 20:18:47 +0900

    /findpasswd고치기

--- routes/1/findpassword.js
+++ routes/1/findpassword.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var app = express();
 var fs = require('fs');
 
-router.post('/findpassword', function(req, res, next){
+router.post('/', function(req, res, next){
 
 	pool.getConnection(function(error, connection){
     	if (error){
@@ -11,32 +11,52 @@ router.post('/findpassword', function(req, res, next){
       		res.sendStatus(500);
     	}
   		var sql, inserts;
-    	sql = 'update duckmate.member set helpflag = 1 where member_email =?';
+
+		// if문 으로 email이 있는지 없는지 확인
+		sql = 'select * from duckmate.member where member_email =?';
     	inserts = [req.body.member_email ];
 
     	connection.query(sql, inserts, function(error, rows){
-			connection.release();
-
 			console.log("rows",rows);
 
 	    	if (error){
 	    	  console.log("Connection Error" + error);
 	    	  res.status(500).send({result : "db error"});
 	   		}
-
-
+			// 뭐가 없음  이거
 			if( rows[0] == undefined ){
                 res.status(201).send(
                     { result: false }
+
                 );
                 return
-            }
+            } else { //뭐가 있음 이거
 
-            res.status(201).send(
-                { result: true }
-            );
-            return
- 		});//connection query
+				//있다면 여기
+		    	sql = 'update duckmate.member set helpflag = 1 where member_email =?';
+		    	inserts = [req.body.member_email ];
+
+		    	connection.query(sql, inserts, function(error, rows){
+					connection.release();
+
+					console.log("rows",rows);
+
+			    	if (error){
+			    	  console.log("Connection Error" + error);
+			    	  res.status(500).send({result : "db error"});
+			   		}
+
+		            res.status(201).send(
+		                { result: true }
+		            );
+		            return
+		 		});//connection query
+
+			}
+
+
+		});
+
 
   	});
 })
