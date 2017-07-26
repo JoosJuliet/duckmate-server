e486e0067e38ad7cff303fd899e4259e4c14e947
joowon <saven7788@gmail.com>
2017-07-26 14:52:04 +0900

    #

--- routes/1/register.js
+++ routes/1/register.js
@@ -26,40 +26,74 @@ var app = express();
 // };
 // responseToJson(1,1,1);
 
+var upload = function (req, res) {
+    var deferred = Q.defer();
+    var storage = multer.diskStorage({
+        // 서버에 저장할 폴더
+        destination: function (req, file, cb) {
+            cb(null, imagePath);
+        },
 
-
-
-
-router.post('/',function(req, res, next){
-// db에서 notnull인 것 없을 때 예외처리 다 해야한다
-
-// member_img,member_email,member_passwd, member_name
-// member_img은 member_email로 이름을 만든다.
-
-
-    pool.getConnection(function(error, connection){
-        if (error){
-                console.log("getConnection Error" + error);
-                res.sendStatus(500);
+        // 서버에 저장할 파일 명
+        filename: function (req, file, cb) {
+            file.uploadedFile = {
+                name: req.params.filename,
+                ext: file.mimetype.split('/')[1]
+            };
+            cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
         }
-        var sql = 'insert into duckmate.member(member_email, member_passwd, member_name) values(?,?,?)';
-        var inserts = [ req.body.member_email, req.body.member_passwd, req.body.member_name];
-        connection.query(sql, inserts, function(error, rows){
-            connection.release();
-            if (error){
-              console.log("register에서 post / Connection Error" + error);
-              res.status(500);
-            }
-            console.log( rows );
-        	if( rows.length === 0 ){
-        		res.status(201).send({result: false});
-        	}else{
-                res.status(201).send({result : true});
-        	}
-        });//connection query
+    });
 
+    var upload = multer({ storage: storage }).single('file');
+    upload(req, res, function (err) {
+        if (err) deferred.reject();
+        else deferred.resolve(req.file.uploadedFile);
+    });
+    return deferred.promise;
+};
+
+
+router.post('/:filename', function(req, res, next) {
+    upload(req, res).then(function (file) {
+        res.json(file);
+    }, function (err) {
+        res.send(500, err);
     });
 });
+
+
+
+//
+// router.post('/',function(req, res, next){
+// // db에서 notnull인 것 없을 때 예외처리 다 해야한다
+//
+// // member_img,member_email,member_passwd, member_name
+// // member_img은 member_email로 이름을 만든다.
+//
+//
+//     pool.getConnection(function(error, connection){
+//         if (error){
+//                 console.log("getConnection Error" + error);
+//                 res.sendStatus(500);
+//         }
+//         var sql = 'insert into duckmate.member(member_email, member_passwd, member_name) values(?,?,?)';
+//         var inserts = [ req.body.member_email, req.body.member_passwd, req.body.member_name];
+//         connection.query(sql, inserts, function(error, rows){
+//             connection.release();
+//             if (error){
+//               console.log("register에서 post / Connection Error" + error);
+//               res.status(500);
+//             }
+//             console.log( rows );
+//         	if( rows.length === 0 ){
+//         		res.status(201).send({result: false});
+//         	}else{
+//                 res.status(201).send({result : true});
+//         	}
+//         });//connection query
+//
+//     });
+// });
 
 // TODO 비밀번호 찾고싶은 사람 표시해주기
 
