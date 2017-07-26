58fb0634470f5a91c057e83c16eba3631e0575fb
joowon <saven7788@gmail.com>
2017-07-26 20:28:53 +0900

    #

--- routes/alarm.js
+++ routes/alarm.js
@@ -5,29 +5,45 @@ var router = express.Router();
 
 
 router.post('/',function(req, res, next){
-    pool.getConnection(function(error, connection){
-        if (error){
-                console.log("getConnection Error" + error);
-                res.sendStatus(500);
+    if( !req.body.firebasetoken ){
+        res.json({
+            result: false,
+            msg: "req.body.firebasetoken이 없습니다."
+        });
+        return;
+    }else if( !req.body.today_alarm ){
+        res.json({
+            result: false,
+            msg: "req.body.today_alarm이 없습니다."
+        });
+        return;
+    }else if( !req.body.member_id ){
+        res.json({
+            result: false,
+            msg: "req.body.member_id이 없습니다."
+        });
+        return;
+    }
+
+    pool.query( 'update duckmate.member set firebasToken = ? or today_alarm = ? where member_id = ?;', [ req.body.firebasetoken, req.body.today_alarm,req.body.member_id] , function(err, rows) {
+        if (err){
+            res.json({
+                result: false,
+                msg: "db 접속 에러"
+            });
+            return;
         }
-        console.log(req.body.member_id);
-        console.log(req.body.firebasetoken);
-        console.log(req.body.today_alarm);
-
-        connection.query('update duckmate.member set firebasToken = ? or today_alarm = ? where member_id = ?;', [ req.body.firebasetoken, req.body.today_alarm,req.body.member_id], function(error, results){
-            connection.release();
-            if (error){
-              console.log(" post /alarm/firebasetoken Connection Error" + error);
-              res.status(500);
-            }
-            console.log( results );
-        	if( results.affectedRows === 1 ){
-        		res.status(201).send({result: false});
-        	}else{
-                res.status(201).send({result : true});
-        	}
-        });//connection query
-
+        if( results.affectedRows === 1 ){
+            res.status(201).json({
+                result: true,
+                msg: "업데이트가 완료되었습니다.",
+            });
+        }else{
+            res.status(201).json({
+                result: false,
+                msg: "업데이트를 실패했습니다.",
+            });
+        }
     });
 });
 
