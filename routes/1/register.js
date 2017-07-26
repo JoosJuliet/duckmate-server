ee51ea61f047a077fe7fd0803063699b4b22b2d7
joowon <saven7788@gmail.com>
2017-07-26 14:49:42 +0900

    #

--- routes/1/register.js
+++ routes/1/register.js
@@ -1,3 +1,15 @@
+
+
+/*******img올리기 위해 필요한 것들 ********/
+var fs = require('fs');
+var imagePath = "public/images";
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var Q = require("q");
+/************************************/
+
+
+
 var express = require('express');
 var router = express.Router();
 var app = express();
@@ -14,25 +26,39 @@ var app = express();
 // };
 // responseToJson(1,1,1);
 
-/*******img올리기 위해 필요한 것들 ********/
-var fs = require('fs');
-var imagePath = "public/images";
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
-var Q = require("q");
-/************************************/
+
+
 
 
 router.post('/',function(req, res, next){
-    console.log("1");
-    res.status(201).send(
-        { result: false }
-    );
 // db에서 notnull인 것 없을 때 예외처리 다 해야한다
 
 // member_img,member_email,member_passwd, member_name
 // member_img은 member_email로 이름을 만든다.
 
+
+    pool.getConnection(function(error, connection){
+        if (error){
+                console.log("getConnection Error" + error);
+                res.sendStatus(500);
+        }
+        var sql = 'insert into duckmate.member(member_email, member_passwd, member_name) values(?,?,?)';
+        var inserts = [ req.body.member_email, req.body.member_passwd, req.body.member_name];
+        connection.query(sql, inserts, function(error, rows){
+            connection.release();
+            if (error){
+              console.log("register에서 post / Connection Error" + error);
+              res.status(500);
+            }
+            console.log( rows );
+        	if( rows.length === 0 ){
+        		res.status(201).send({result: false});
+        	}else{
+                res.status(201).send({result : true});
+        	}
+        });//connection query
+
+    });
 });
 
 // TODO 비밀번호 찾고싶은 사람 표시해주기
