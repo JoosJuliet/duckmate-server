e65fea71bbd6531a9c4a2013b575492787e3a529
joowon <saven7788@gmail.com>
2017-08-09 21:21:01 +0900

    #

--- routes/android/singer.js
+++ routes/android/singer.js
@@ -42,7 +42,7 @@ router.route('/')
     });
 })
 .get((req,res)=>{
-    console.log( "1",req.fbpage.m);
+    // console.log( "1",req.fbpage.m); 
 //TODO 에러처리 부족
     if(!req.query.firebaseToken){
         res.json({
