708c167a8cb4954d89fec2d47762fd8eba67b843
joowon <saven7788@gmail.com>
2017-09-02 12:19:14 +0900

    오타수정

--- routes/android/user/register.js
+++ routes/android/user/register.js
@@ -80,9 +80,7 @@ admin.initializeApp({
 
 router.route('/id')
 .get((req, res)=>{
-	pool.query( 'select * from duckmate.member ', [ req.query.uid ] , function( err, rows ) {
-
-		// 'select FirebaseToken from duckmate.member where firebaseUid = ?', [ req.query.uid ] , function( err, rows ) {
+	pool.query( 'select firebaseToken from duckmate.member where firebaseUid = ?', [ req.query.uid ] , function( err, rows ) {
 		if (err){
 			console.log(err);
 			res.json({
@@ -94,12 +92,12 @@ router.route('/id')
 		}
 		console.log('rows',rows);
 		console.log('rows[0]',rows[0]);
-		// console.log('rows[0].FirebaseToken',rows[0].FirebaseToken);
+		console.log('rows[0].FirebaseToken',rows[0].firebaseToken);
 		if( rows.length === 1 ){
 			res.status(201).json({
 				result: true,
 				msg: "firebasToken입니다.",
-				data : rows[0]
+				data : rows[0].firebaseToken
 			});
 		}else{
 			res.status(201).json({
