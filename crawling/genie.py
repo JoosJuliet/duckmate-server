85a5d7714f295b5f37a053265447c75ae4c13c40
joos <joos@smartstudy.com@gmail.com>
2018-05-11 21:30:13 +0900

    [UPDATE] crawling 보기

--- crawling/genie.py
+++ crawling/genie.py
@@ -1,32 +1,21 @@
 import pymysql.cursors
 import requests
 from bs4 import BeautifulSoup
-
-
-html2 = requests.get('http://www.genie.co.kr/chart/top100?pg=2').text
-html = requests.get('http://www.genie.co.kr/chart/top100').text
-
+html2 = requests.get('http://www.genie.co.kr/chart/top200?pg=2').text
+html = requests.get('http://www.genie.co.kr/chart/top200').text
 soup = BeautifulSoup(html, 'html.parser')
 soup2 = BeautifulSoup(html2, 'html.parser')
-
-
 tag_list = []
 tag_list2 = []
-
 for tr_tag in soup.find('tbody').find_all('tr'):
-	tag = tr_tag.find_all(class_='title ellipsis')
-
-	if tag:
-		tag_sub_list = tag
-		tag_list.extend(tag_sub_list)
-
+        tag = tr_tag.find_all(class_='title ellipsis')
+        if tag:
+                tag_sub_list = tag
+                tag_list.extend(tag_sub_list)
 for tr_tag2 in soup2.find('tbody').find_all('tr'):
-	tag = tr_tag2.find_all(class_='title ellipsis')
-
-	if tag:
-		tag_sub_list = tag
-		tag_list.extend(tag_sub_list)
-
+        tag = tr_tag2.find_all(class_='title ellipsis')
+        if tag:
+                tag_sub_list = tag
+                tag_list.extend(tag_sub_list)
 for idx in tag_list:
         print(idx.text)
-
--- crawling/melon.py
+++ crawling/melon.py
@@ -1,47 +1,37 @@
 import pymysql.cursors
 import requests
 from bs4 import BeautifulSoup
-
 conn = pymysql.connect(
-	host='localhost',
-	user='root',
-	password='root',
-	db='duckmate',
-	charset='utf8mb4')
+        host='localhost',
+        user='root',
+        password='root',
+        db='duckmate',
+        charset='utf8mb4')
 
- 
 html = requests.post('http://www.melon.com/chart/real/index.htm').text
 soup = BeautifulSoup(html, 'html.parser')
-
-
 tag_list = []
 tag_list2 = []
-
 for tr_tag in soup.find('tbody').find_all('tr'):
     tag = tr_tag.find(class_='wrap_song_info')
     if tag:
         tag_sub_list = tag.find_all(href=lambda value: (value and 'playSong' in value))
         tag_list.extend(tag_sub_list)
-
     if tag:
-	tag_sub_list2 = tag.find(class_='ellipsis rank02').find_all(class_='checkEllipsis')
-	tag_list2.extend(tag_sub_list2)
-
-
+        tag_sub_list2 = tag.find(class_='ellipsis rank02').find_all(class_='checkEllipsis')
+        tag_list2.extend(tag_sub_list2)
 num=0
-
 for idx in tag_list:
-	print(idx.text)
-
+        print(idx.text)
 for idx in tag_list2:
-	num = num+1
-	print(num)
-	print(idx.text)
-
-'''
-curs = conn.cursor(pymysql.cursors.DictCursor)
-sql = ""
-sql += "update chart_melon set song = '"
-'''
-
-
+        num = num+1
+        print(num)
+        print(idx.text)
+try:
+    with conn.cursor() as cursor:
+        sql = 'UPDATE chart_melon SET singer_name = \'test\' WHERE idx=\'1\''
+        cursor.execute(sql)
+    conn.commit()
+    print(cursor.rowcount) # 1 (affected rows)
+finally:
+    conn.close()
